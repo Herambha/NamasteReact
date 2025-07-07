b8c3f0e30f98118d766a7cbc3f4dc125f7e96864
Herambha <42058918+Herambha@users.noreply.github.com>
2025-07-07 22:19:47 +0530

    init

---
+++ App.js
@@ -0,0 +1,24 @@
+const element = React.createElement("div", {
+    id: "parent"
+}, [React.createElement("div", {
+    id: "child1"
+}, [React.createElement("h1", {
+    id: "h1"
+}, "Hello! This is H1 Tag of Child1"),
+React.createElement("h2", {
+    id: "h2"
+}, "Hello! This is H2 Tag of Child1")]
+),React.createElement("div", {
+    id: "child2"
+}, [React.createElement("h1", {
+    id: "h1"
+}, "Hello! This is H1 Tag of Child2"),
+React.createElement("h2", {
+    id: "h2"
+}, "Hello! This is H2 Tag of Child2")]
+)]
+);
+console.log(element);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(root);
+root.render(element);
