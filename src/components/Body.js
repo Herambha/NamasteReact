767e47a9b64c0faabc2a854aea965910220c4620
Aditya <adig6281@gmail.com>
2025-08-07 09:24:12 +0530

    react hook added

---
+++ src/components/Body.js
@@ -0,0 +1,27 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useState } from "react";
+const Body = () => {
+    const [restaurantList, setRestaurantList] = useState(resList)
+    return (
+        <div className="body">
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    const updatedList = restaurantList.filter(
+                        (res) => res.card.card.info.avgRating > 4
+                    )
+                    setRestaurantList(updatedList);
+                }} >Top Rated Restaurants</button>
+            </div>
+            <div className="res-container">
+                {
+                    restaurantList.map((restaurant) => (
+                        <RestaurantCard key={restaurant.card.card.info.id} resData={restaurant}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/utils/mockData.js
@@ -0,0 +1,411 @@
+const resList = [
+    {
+        card: {
+        card: {
+        info: {
+        id: "416611",
+        name: "Cafe Mazda Restaurant",
+        cloudinaryImageId: "jtzgqi46lxr5e1klb7k6",
+        locality: "Grant Road",
+        areaName: "Chowpatty",
+        costForTwo: "₹400 for two",
+        cuisines: [
+        "Indian",
+        "Chinese",
+        "Mughlai"
+        ],
+        avgRating: 3.7,
+        parentId: "231788",
+        avgRatingString: "3.7",
+        totalRatingsString: "751",
+        promoted: true,
+        adTrackingId: "cid=fbcea5b9-7ee3-4da7-9dce-6b80be72210b~p=0~adgrpid=fbcea5b9-7ee3-4da7-9dce-6b80be72210b#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=416611~plpr=COLLECTION~eid=ccefb423-fe83-4eec-8604-e4d2891c18ec~srvts=1754369553698~collid=80475",
+        sla: {
+        deliveryTime: 24,
+        lastMileTravel: 1.7,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "1.7 km",
+        iconType: "ICON_TYPE_EMPTY"
+        },
+        availability: {
+        nextCloseTime: "2025-08-05 23:59:00",
+        opened: true
+        },
+        badges: { },
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+        entityBadges: {
+        textBased: { },
+        imageBased: { },
+        textExtendedBadges: { }
+        }
+        },
+        aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹59",
+        discountCalloutInfo: {
+        message: "Free Delivery",
+        logoCtx: {
+        logo: "v1655895371/free_delivery_logo_hqipbo.png"
+        }
+        },
+        logoCtx: {
+        text: "BENEFITS"
+        }
+        },
+        orderabilityCommunication: {
+        title: { },
+        subTitle: { },
+        message: { },
+        customIcon: { },
+        commsStyling: { }
+        },
+        differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: { },
+        video: { }
+        }
+        },
+        reviewsSummary: { },
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: { },
+        externalRatings: {
+        aggregatedRating: {
+        rating: "3.8",
+        ratingCount: "186"
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag"
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        campaignId: "fbcea5b9-7ee3-4da7-9dce-6b80be72210b"
+        },
+        analytics: { },
+        cta: {
+        link: "swiggy://menu?restaurant_id=416611&source=collection&query=Paratha",
+        text: "RESTAURANT_MENU",
+        type: "DEEPLINK"
+        },
+        widgetId: "collectionV5RestaurantListWidget"
+        }
+        }
+        },
+        {
+        card: {
+        card: {
+        info: {
+        id: "443952",
+        name: "Aditi Nx",
+        cloudinaryImageId: "qlebahepktqicbfko7sk",
+        locality: "Connaught Place",
+        areaName: "Parel",
+        costForTwo: "₹400 for two",
+        cuisines: [
+        "South Indian",
+        "North Indian",
+        "Healthy Food",
+        "Juices",
+        "Jain",
+        "Beverages",
+        "Ice Cream"
+        ],
+        avgRating: 4.4,
+        parentId: "267252",
+        avgRatingString: "4.4",
+        totalRatingsString: "1.0K+",
+        promoted: true,
+        adTrackingId: "cid=4df64412-0a7a-4c84-8635-118a3e523d55~p=7~adgrpid=4df64412-0a7a-4c84-8635-118a3e523d55#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=443952~plpr=COLLECTION~eid=85c53f8b-430a-47af-8ece-089a111ae2ee~srvts=1754369553698~collid=80475",
+        sla: {
+        deliveryTime: 52,
+        lastMileTravel: 5.6,
+        serviceability: "SERVICEABLE",
+        slaString: "50-60 mins",
+        lastMileTravelString: "5.6 km",
+        iconType: "ICON_TYPE_EMPTY"
+        },
+        availability: {
+        nextCloseTime: "2025-08-05 23:00:00",
+        opened: true
+        },
+        badges: { },
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+        entityBadges: {
+        textBased: { },
+        imageBased: { },
+        textExtendedBadges: { }
+        }
+        },
+        aggregatedDiscountInfoV3: {
+        discountCalloutInfo: {
+        message: "Free Delivery",
+        logoCtx: {
+        logo: "v1655895371/free_delivery_logo_hqipbo.png"
+        }
+        },
+        logoCtx: {
+        text: "BENEFITS"
+        }
+        },
+        orderabilityCommunication: {
+        title: { },
+        subTitle: { },
+        message: { },
+        customIcon: { },
+        commsStyling: { }
+        },
+        differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: { },
+        video: { }
+        }
+        },
+        reviewsSummary: { },
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: { },
+        externalRatings: {
+        aggregatedRating: {
+        rating: "4.4",
+        ratingCount: "644"
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag"
+        },
+        ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+        campaignId: "4df64412-0a7a-4c84-8635-118a3e523d55"
+        },
+        analytics: { },
+        cta: {
+        link: "swiggy://menu?restaurant_id=443952&source=collection&query=Paratha",
+        text: "RESTAURANT_MENU",
+        type: "DEEPLINK"
+        },
+        widgetId: "collectionV5RestaurantListWidget"
+        }
+        }
+        },
+        {card: {
+            card: {
+            info: {
+            id: "411941",
+            name: "Oye Kiddan",
+            cloudinaryImageId: "564cc6cfeaec66b24178447a9a1a2afd",
+            locality: "Mint Road",
+            areaName: "FortColaba",
+            costForTwo: "₹300 for two",
+            cuisines: [],
+            avgRating: 4.2,
+            parentId: "690",
+            avgRatingString: "4.2",
+            totalRatingsString: "5.0K+",
+            promoted: true,
+            adTrackingId: "cid=6e59c347-20cb-4db9-8fe8-56e94d3156b6~p=2~adgrpid=6e59c347-20cb-4db9-8fe8-56e94d3156b6#ag1~mp=SWIGGY_IN~bl=FOOD~aet=RESTAURANT~aeid=411941~plpr=COLLECTION~eid=1d55049d-4b93-497b-8fd6-9135da4eb33d~srvts=1754369553698~collid=80475",
+            sla: {
+            deliveryTime: 42,
+            lastMileTravel: 2.5,
+            serviceability: "SERVICEABLE",
+            slaString: "40-45 mins",
+            lastMileTravelString: "2.5 km",
+            iconType: "ICON_TYPE_EMPTY"
+            },
+            availability: {
+            nextCloseTime: "2025-08-05 15:45:00",
+            opened: true
+            },
+            badges: {
+            textExtendedBadges: [
+            {
+            iconId: "guiltfree/GF_Logo_android_3x",
+            shortDescription: "options available",
+            fontColor: "#7E808C"
+            }
+            ]
+            },
+            isOpen: true,
+            type: "F",
+            badgesV2: {
+            entityBadges: {
+            textBased: { },
+            imageBased: { },
+            textExtendedBadges: {
+            badgeObject: [
+            {
+            attributes: {
+            shortDescription: "options available",
+            fontColor: "#7E808C",
+            iconId: "guiltfree/GF_Logo_android_3x",
+            description: ""
+            }
+            }
+            ]
+            }
+            }
+            },
+            aggregatedDiscountInfoV3: {
+            header: "50% OFF",
+            discountTag: "FLAT DEAL",
+            discountCalloutInfo: {
+            message: "Free Delivery",
+            logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png"
+            }
+            },
+            logoCtx: {
+            text: "BENEFITS"
+            }
+            },
+            orderabilityCommunication: {
+            title: { },
+            subTitle: { },
+            message: { },
+            customIcon: { },
+            commsStyling: { }
+            },
+            differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: { },
+            video: { }
+            }
+            },
+            reviewsSummary: { },
+            displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            restaurantOfferPresentationInfo: { },
+            externalRatings: {
+            aggregatedRating: {
+            rating: "--"
+            }
+            },
+            ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+            campaignId: "6e59c347-20cb-4db9-8fe8-56e94d3156b6"
+            },
+            analytics: { },
+            cta: {
+            link: "swiggy://menu?restaurant_id=411941&source=collection&query=Paratha",
+            text: "RESTAURANT_MENU",
+            type: "DEEPLINK"
+            },
+            widgetId: "collectionV5RestaurantListWidget"
+            }
+            }},
+            {
+            card: {
+            card: {
+            info: {
+            id: "33660",
+            name: "Shiv Sagar",
+            cloudinaryImageId: "9d61b45575cdfe2281a31d7927260a95",
+            locality: "Chowpatty",
+            areaName: "Chowpatty",
+            costForTwo: "₹400 for two",
+            cuisines: [
+            "South Indian",
+            "North Indian",
+            "Chinese",
+            "Biryani"
+            ],
+            avgRating: 4.5,
+            veg: true,
+            parentId: "845",
+            avgRatingString: "4.5",
+            totalRatingsString: "13K+",
+            sla: {
+            deliveryTime: 28,
+            lastMileTravel: 0.9,
+            serviceability: "SERVICEABLE",
+            slaString: "25-30 mins",
+            lastMileTravelString: "0.9 km",
+            iconType: "ICON_TYPE_EMPTY"
+            },
+            availability: {
+            nextCloseTime: "2025-08-05 23:00:00",
+            opened: true
+            },
+            badges: {
+            imageBadges: [
+            {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!"
+            }
+            ]
+            },
+            isOpen: true,
+            type: "F",
+            badgesV2: {
+            entityBadges: {
+            imageBased: {
+            badgeObject: [
+            {
+            attributes: {
+            imageId: "android/static-assets/icons/big_rx.png",
+            description: "bolt!"
+            }
+            }
+            ]
+            },
+            textExtendedBadges: { },
+            textBased: { }
+            }
+            },
+            aggregatedDiscountInfoV3: {
+            header: "10% OFF",
+            discountTag: "FLAT DEAL",
+            discountCalloutInfo: {
+            message: "Free Delivery",
+            logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png"
+            }
+            },
+            logoCtx: {
+            text: "BENEFITS"
+            }
+            },
+            orderabilityCommunication: {
+            title: { },
+            subTitle: { },
+            message: { },
+            customIcon: { },
+            commsStyling: { }
+            },
+            differentiatedUi: {
+            displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+            differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: { },
+            video: { }
+            }
+            },
+            reviewsSummary: { },
+            displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            restaurantOfferPresentationInfo: { },
+            externalRatings: {
+            aggregatedRating: {
+            rating: "4.0",
+            ratingCount: "1.3K+"
+            },
+            source: "GOOGLE",
+            sourceIconImageId: "v1704440323/google_ratings/rating_google_tag"
+            },
+            ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+            },
+            analytics: { },
+            cta: {
+            link: "swiggy://menu?restaurant_id=33660&source=collection&query=Paratha",
+            text: "RESTAURANT_MENU",
+            type: "DEEPLINK"
+            },
+            widgetId: "collectionV5RestaurantListWidget"
+            }
+            }
+            },
+]
+
+export default resList;
